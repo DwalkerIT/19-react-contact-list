656b3ab654a00f6cabadd1061f7c1fd08c3c7d04
DWalkerIT <DWalkerIT@gmail.com>
2021-03-09 11:46:08 -0500

    icons not working

--- contact/src/pages/ContactDetail.js
+++ contact/src/pages/ContactDetail.js
@@ -12,12 +12,21 @@ export default function ContactDetail(props) {
       <Link to="/">
         <h3>back</h3>
       </Link>
+      <div>
+        <img className="singleImage" src={contact.picture.thumbnail}></img>
+      </div>
       <p>
+        <i class="fas fa-user"></i>
         {contact.name.title} {contact.name.first} {contact.name.last}
       </p>
-      <p>{contact.email}</p>
+      <p>
+        <i class="fas fa-envelope"></i>
+        {contact.email}
+      </p>
+      <i class="fas fa-mobile"></i>
       <a href={"tel:" + contact.phone}>{contact.phone}</a>
       <p>
+        <i class="fas fa-globe-americas"></i>
         {contact.location.city} {contact.location.state} {contact.location.zip}{" "}
         {contact.location.country}
       </p>
